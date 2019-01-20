5909ace1dd935d43e6530ded85010886bb84c397
Valerii <pp000302@gmail.com>
2019-01-20 05:29:49 +0300

    Обновлен 20.01.2019 3:09

---
+++ src/Tsicli_Recursia/Zadacha6.java
@@ -0,0 +1,23 @@
+/*Имеется число. Посчитать сумму цифр данного числа.*/
+package Tsicli_Recursia;
+
+import java.util.Scanner;
+import static java.lang.System.out;
+
+public class Zadacha6 {
+    public static void main(String[] args) {
+
+        Scanner sr =new Scanner(System.in);
+        out.print("ВВедите число ");
+        String n= sr.nextLine();
+        int lng=n.length();
+        int sum=0;
+
+        for (int i = 0; i < lng; i++) {
+            sum+=Character.getNumericValue(n.charAt(i));
+
+        }
+
+        out.println("Сумма цифр числа равна "+sum);
+    }
+}
