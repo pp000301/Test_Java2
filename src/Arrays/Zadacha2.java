aeaa4e11bf60ef5a159fe2d840e99c6eac8f8de7
Artyom <artyom.yavorski@gmail.com>
2019-01-21 20:33:37 +0300

    Добавление задач с массивом

--- src/Arrays/Zadacha2.java
+++ src/Arrays/Zadacha2.java
@@ -1,7 +1,19 @@
 package Arrays;
 
+import static java.lang.System.out;
+
+
 public class Zadacha2 {
+    private static void invert(int n, int[] a) {
+
+        for (int k = n - 1; k >= 0; k--) {
+            out.println(a[k]);
+        }
+    }
+
     public static void main(String[] args) {
+
+
 
     }
 
