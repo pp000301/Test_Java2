79f832ed7aa54b51109e7a7a2744cc26521d992c
Валерий <pp000302@gmail.com>
2019-01-24 21:44:23 +0300

    обновлен пакет строки

--- src/Arrays/Zadacha2.java
+++ src/Arrays/Zadacha2.java
@@ -1,29 +1,11 @@
 /*Напишите программу, заносящую в массив первые 100 натуральных чисел, делящихся на 13 или на 17, и печатающую его.*/
 package Arrays;
 
-import java.util.Scanner;
 
 import static java.lang.System.out;
 
 
 public class Zadacha2 {
-
-
-    private static void sequence(int[] a) {
-
-        out.println(" элементы массива в обычном порядке ");
-        for (int i = 0; i < 100; i++) {
-            out.println(a[i]);
-        }
-    }
-
-    public static boolean Del1(int a) {
-
-        Boolean result = false;
-        if ((a % 13 == 0) | (a % 17 == 0))
-            result = true;
-        return result;
-    }
 
 
     public static void main(String[] args) {
@@ -42,5 +24,22 @@ public class Zadacha2 {
 
 
     }
+
+    private static void sequence(int[] a) {
+
+        out.println(" элементы массива в обычном порядке ");
+        for (int i = 0; i < 100; i++) {
+            out.println(a[i]);
+        }
+    }
+
+    public static boolean Del1(int a) {
+
+        Boolean result = false;
+        if ((a % 13 == 0) | (a % 17 == 0))
+            result = true;
+        return result;
+    }
+
 
 }
--- src/String/Zadacha5.java
+++ src/String/Zadacha5.java
@@ -1,43 +1,24 @@
-package String;
 /*Заданы два массива строк. Написать функцию, определяющую, является ли содержимое этих массивов идентичным.
-Порядок расположения элементов значения не имеет. */
+Порядок расположения элементов значения не имеет.*/
 
-import java.util.Arrays;
-import java.util.Scanner;
+package String;
+
+import java.util.*;
 
 import static java.lang.System.out;
 
 public class Zadacha5 {
+
     public static void main(String[] args) {
 
-        Scanner sr = new Scanner(System.in);
-        out.println("ВВедите количество элементов первого массива  ");
-        int n = sr.nextInt();
-        String[] a = new String[4];
+        String[] a = {"1", "2"};
+        String[] b = {"2", "3"};
 
-        // бЛОК ВВОДА4
-        out.print("ВВедите элементы первого массива ");
-    a[0]="8";
-        for (int i = 0; i < n; i++) {
-            a[i] = sr.nextLine();
-        }
+        Arrays.sort(a);
+        Arrays.sort(b);
 
-        //out.println("ВВедите количество элементов Второго массива  ");
-        //int l = sr.nextInt();
-        //String[] b = new String[3];
-
-        // бЛОК ВВОДА
-        //out.println("ВВедите элементы второго массива ");
-        //for (int i = 0; i < l; i++) {
-            //b[i] = sr.nextLine();
-        //}
-
-        //Arrays.sort(a);
-        //Arrays.sort(b);
-
-        for (int i = 0; i < n; i++) {
-            out.println(a[i]);
-        }
+        boolean resuil = Arrays.deepEquals(a, b);
+        out.println(resuil);
 
 
     }
