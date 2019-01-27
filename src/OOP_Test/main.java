6848d745275db6d8933594408c775cb527e956bd
Valerii <pp000302@gmail.com>
2019-01-27 15:24:51 +0300

    Обновлен 20.01.2019 3:09

--- src/OOP_Test/main.java
+++ src/OOP_Test/main.java
@@ -1,13 +1,20 @@
 package OOP_Test;
 
+import OOP_Test.HW.Car;
 import OOP_Test.HW.Point;
 
 public class main {
     public static void main(String[] args) {
         //Time time=new Time(12,25,36);
         //time.printTime();
-        Point point=new Point(3,4).translate(1,2).scale(2);
-        point.get();
+        Car car=new Car(10,0);
+        car.getInfo();
+        car.toLockIn(20);
+        car.getInfo();
+        car.toLockIn(20);
+        car.getInfo();
+        car.run(200);
+        car.getInfo();
 
 
     }
