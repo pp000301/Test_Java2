f0cb311c0d52b4fea903eaa3467e0acb63bb2814
Валерий <pp000302@gmail.com>
2019-01-27 14:14:24 +0300

    обновление oop

---
+++ src/OOP_Test/main.java
@@ -0,0 +1,14 @@
+package OOP_Test;
+
+import OOP_Test.HW.Point;
+
+public class main {
+    public static void main(String[] args) {
+        //Time time=new Time(12,25,36);
+        //time.printTime();
+        Point point=new Point(3,4).translate(1,2).scale(2);
+        point.get();
+
+
+    }
+}
