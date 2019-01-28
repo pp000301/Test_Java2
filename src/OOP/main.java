11ab9121d2512d09fd2e4aa27b65b1571b4311d1
Artyom <artyom.yavorski@gmail.com>
2019-01-28 20:38:29 +0300

    Обновление ООП

--- src/OOP/main.java
+++
@@ -1,8 +0,0 @@
-package OOP;
-
-public class main {
-    public static void main(String[] args) {
-        Time time = new Time(12,12,12);
-        time.printTime();
-    }
-}
--- src/OOP_Test/main.java
+++ src/OOP_Test/main.java
@@ -2,19 +2,28 @@ package OOP_Test;
 
 import OOP_Test.HW.Car;
 import OOP_Test.HW.Point;
+import OOP_Test.encapsulation.Bancomat;
 
 public class main {
     public static void main(String[] args) {
         //Time time=new Time(12,25,36);
         //time.printTime();
-        Car car=new Car(10,0);
-        car.getInfo();
-        car.toLockIn(20);
-        car.getInfo();
-        car.toLockIn(20);
-        car.getInfo();
-        car.run(200);
-        car.getInfo();
+        //Car car=new Car(10,0);
+        //car.getInfo();
+        //car.toLockIn(20);
+       // car.getInfo();
+        //car.toLockIn(20);
+        //car.getInfo();
+        //car.run(200);
+       // car.getInfo();
+
+        Bancomat bancomat=new Bancomat(10,10,10);
+        bancomat.setMoney10(1000);
+        bancomat.setMoney50(100);
+        bancomat.getIhformation();
+        bancomat.takeMoney(100);
+        bancomat.getIhformation();
+
 
 
     }
