702db2b7a2f28e7a04e79c8502d7e6a94ac1160a
Valerii <pp000302@gmail.com>
2019-02-19 22:06:01 +0300

    Обновлен 20.01.2019 3:09

--- src/regular_expressions/main.java
+++ src/regular_expressions/main.java
@@ -4,25 +4,29 @@ package regular_expressions;
 
 
 import java.io.*;
-import java.nio.file.*;
+
 
 public class main {
     public static void main(String[] args) {
-        //String basePath = new File("").getAbsolutePath();
-        Path basePath = (Path) FileSystems.getDefault();
-        System.out.println(basePath);
-        try{
-            FileInputStream fstream = new FileInputStream("text.txt");
-            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-            String strLine;
-            while ((strLine = br.readLine()) != null){
-                System.out.println(strLine);
+
+        try(FileInputStream fin=new FileInputStream("E://text.txt"))
+        {
+            StringBuffer strBuffer = new StringBuffer();
+            int i=-1;
+            while((i=fin.read())!=-1){
+                strBuffer.append((char)i);
             }
-        }catch (IOException e){
-            System.out.println("Ошибка");
+            System.out.println(strBuffer);
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+
         }
 
+       final byte a=2;
+
     }
+
 
 
 }
