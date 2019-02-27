70ce4672524ad8423b56dacdc2abb6d14c440568
Ars199 <krukaa85@tut.by>
2019-02-27 21:34:10 +0300

    concurent

--- src/concurrent/Councurent2_2.java
+++ src/concurrent/Councurent2_2.java
@@ -1,6 +1,6 @@
 package concurrent;
 
-
+import java.util.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,26 +8,61 @@ import java.util.Scanner;
 import static java.lang.System.out;
 
 public class Councurent2_2 {
+    private static final Object monitor = new Object();
+    private static final List<String> list = new ArrayList<>();
 
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+    public static void main(String[] args) throws InterruptedException {
+        input();
 
-        boolean k=true;
-        while (k) {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {e.printStackTrace();}
+
+                synchronized (monitor) {
+                    Collections.sort(list);
+                }
+            }
+        }).start();
+
+        //out.println(list);
+    }
+
+    private static void input() throws InterruptedException {
+
+        while (true) {
             Scanner sr = new Scanner(System.in);
-            out.println("ВВедите строку ");
-            list.add(sr.nextLine());
-            if(sr.nextLine().equals("exit")){
-                k=false;
-                sr.close();
+
+            String n = sr.nextLine();
+            if(n.equals("")){
+                out.println(list);
+            }
+            else {
+                //synchronized (monitor) {
+
+                    list.add(n);
+
+                    //int leng = n.length();
+
+                    //if (leng < 80)
+                    // list.add(n);
+                    //else {
+                    //  list.add(n.substring(0, 80));
+                    //  list.add(n.substring(80, leng));
+                    // }
+
+                    // if (n.equals("exit")) {
+                    //  k = false;
+                    //  sr.close();
+                    // }
+                //}
             }
 
+        }
     }
 
 
-
-
-    }
 
 
 }
