b4c283483eedfe8b80bb9b3335d92d9628af5765
pp000301 <pp000302@gmail.com>
2019-03-02 13:53:23 +0300

    concurent
    Signed-off-by: pp000301 <pp000302@gmail.com>

--- src/concurrent/Concurent4.java
+++ src/concurrent/Concurent4.java
@@ -4,16 +4,15 @@ package concurrent;
 public class Concurent4 {
     public static void main(String[] args) throws InterruptedException {
 
-        Thread thread=new Thread(()->{
-            while (true)  System.out.println("shdghasgdhasghd");
+        Thread thread = new Thread(() -> {
+            while (true) System.out.println("shdghasgdhasghd");
         });
 
         thread.start();
-        for (int i = 0; i <10000 ; i++) {
+        for (int i = 0; i < 10000; i++) {
 
         }
         thread.interrupt();
-
 
 
     }
