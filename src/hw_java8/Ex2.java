9ca1dd01ba2e4e1eb57ef2dadb7e875eee67b8a7
Valerii <pp000302@gmail.com>
2019-03-09 21:57:25 +0300

    Обновлен 20.01.2019 3:09

--- src/hw_java8/Ex2.java
+++ src/hw_java8/Ex2.java
@@ -1,28 +1,20 @@
 package hw_java8;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 public class Ex2 {
     public static void main(String[] args) {
         List<String> names=new ArrayList<>();
-        Collections.addAll(names, "John", "Paul", "George", "John", "Paul", "John")
-        names.stream().
-                distinct().
-                map(p-> p+"-"+cont((Stream<String>) names,p)
-                ).
-                forEach(System.out::println);
+        Collections.addAll(names, "John", "Paul", "George", "John", "Paul", "John");
+        Map<String, Long> counts =
+        names.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+        System.out.println(counts);
 
     }
 
-    static int cont(Stream<String> stream, String s ){
-        int i=0;
-        stream.filter(p->p.equals(s)).peek(i++);
 
-        return i;
-    }
 
 }
