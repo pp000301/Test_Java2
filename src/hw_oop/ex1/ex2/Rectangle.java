c44212092c68f30c9265130a74143ef9c9f5c906
Valerii <pp000302@gmail.com>
2019-04-03 21:50:18 +0300

    Default Changelist

--- src/hw_oop/ex1/ex2/Rectangle.java
+++ src/hw_oop/ex1/ex2/Rectangle.java
@@ -28,12 +28,4 @@ public class Rectangle extends Point implements Figures {
     }
 
 
-    public static class main {
-        public static void main(String[] args) {
-            Circle circle = new Circle(2, 3, 20);
-            Rectangle rectangle = new Rectangle(5, 5, 25, 30);
-            System.out.println("Площать прямоугольника равна" + rectangle.getSquare() + " Диагональ " + rectangle.diagonall());
-            System.out.println("Площадь круга равна " + circle.getSquare());
-        }
-    }
 }
--- src/hw_oop/ex1/ex2/main.java
+++ src/hw_oop/ex1/ex2/main.java
@@ -1,16 +1,16 @@
 package hw_oop.ex1.ex2;
 
-import hw_oop.ex1.Car;
+
 
 public class main {
     public static void main(String[] args) {
 
-Car car=new Car(10,10);
+        Circle circle = new Circle(2,2,10);
+        System.out.println(circle.getSquare());
+        circle.getInformation();
 
-car.getInfo();
-car.setFuelLevel(20);
-        car.getInfo();
-
-
+        Rectangle rectangle = new Rectangle(2,2,4,5);
+        System.out.println("Диагональ четырехугольника "+ rectangle.diagonall());
+        System.out.println("Площадь четырехугольника "+rectangle.getSquare());
     }
 }
